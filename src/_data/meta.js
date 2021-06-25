cd6dcf71119d2cedcb1d148552385d14261c1807
Robert James Gabriel <robert_gabriel@outlook.com>
2021-06-25 15:33:25 +0100

    X

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -2,7 +2,7 @@ module.exports = {
   url: process.env.URL || "http://localhost:8080",
   siteName: "Coffee & Fun - The makers of popular browser extensions.",
   siteDescription:
-    "Quickly launch an 11ty-generated static site. Includes a minimal Sass framework, and generated sitemap, RSS feed, and social share preview images.",
+    "Coffee and Fun make popular useful web browser extensions. From Markdown editors, Sex education, Developer tools, and more.",
   authorName: "Robert James Gabriel",
   twitterUsername: "coffee&fun", // no `@`
 };
