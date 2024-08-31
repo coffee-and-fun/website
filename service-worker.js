4bdc54b94a863688a2c0c8798a11326b11dc8a06
RobertJGabriel <robert_gabriel@outlook.com>
2024-08-31 20:56:16 +0000

    Deploying to gh-pages from @ coffee-and-fun/website@bc7efa244a9cf3b0e22623c5e460f1de1bc467f9 🚀

--- service-worker.js
+++ service-worker.js
@@ -184,7 +184,7 @@ self.__precacheManifest = [
   },
   {
     "url": "forever-advert/index.html",
-    "revision": "8d4cb199ec589b0d2ead4be53c73c194"
+    "revision": "9a2b748efe8f1c3c3aaa4fa98ae38064"
   },
   {
     "url": "index.html",
