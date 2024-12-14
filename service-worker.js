791f86a8e69ab1948588f6ca3eb3302e9e42b39a
RobertJGabriel <robert_gabriel@outlook.com>
2024-12-14 05:29:06 +0000

    Deploying to gh-pages from @ coffee-and-fun/website@ba94aeb255f09ebe961179d07980853577da92cd 🚀

--- service-worker.js
+++ service-worker.js
@@ -332,7 +332,7 @@ self.__precacheManifest = [
   },
   {
     "url": "blog/how-psa-grades-pokemon-cards-research-guide/index.html",
-    "revision": "480d268c27831b7ac83e34f5781c034b"
+    "revision": "093d2c6b56ed6af840602149babbacea"
   },
   {
     "url": "blog/how-to-change-battery-in-an-apple-airtag/index.html",
