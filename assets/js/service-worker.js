b4cbdfa288628e3b0d6017247a3ae05de825cad8
RobertJGabriel <robert_gabriel@outlook.com>
2025-04-21 16:17:09 +0000

    Deploying to gh-pages from @ coffee-and-fun/website@268924b7ac48a2b101ccbddab51ddb33f619dc91 🚀

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -5,10 +5,37 @@ self.addEventListener('install', (event) => {
     fetch('/cache-assets.json')
       .then(res => res.json())
       .then(files => {
+        files.push('/cache-assets.json'); // also cache this file itself
         return caches.open(CACHE_NAME).then(cache => {
           return cache.addAll(files);
         });
       })
+      .catch((err) => {
+        console.log('[SW] Failed to cache files on install:', err);
+      })
   );
   self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) =>
+      Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
